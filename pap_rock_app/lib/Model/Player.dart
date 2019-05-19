5a880b5aaaea62705905d1db0aa35a7941cccf35
paulkastel <pkastelik@poczta.fm>
2019-05-19 17:37:12 +0200

    Added Player model

---
+++ pap_rock_app/lib/Model/Player.dart
@@ -0,0 +1,25 @@
+
+/// Enum for player sex to choose
+enum PlayerSex { Male, Female }
+
+/**
+ * Class that represents user. His name, sex, score etc.
+ */
+class Player {
+  Player(String userName, PlayerSex userSex) {
+    _name = userName;
+    _sex = userSex;
+  }
+
+  String _name;
+  
+  String get playerName => _name;
+  set playerName(String name) {
+    if (name.length > 5) {
+      _name = name;
+    }
+  }
+
+  PlayerSex _sex;
+  PlayerSex get playerSex => _sex;
+}
