675b540ef6c7017ae01a9feeca3212751b90e9cc
paulkastel <pkastelik@poczta.fm>
2019-05-20 23:25:15 +0200

    Added new background gradient, removed appbar

--- pap_rock_app/lib/View/LoginPageView.dart
+++ pap_rock_app/lib/View/LoginPageView.dart
@@ -12,7 +12,8 @@ class LoginPageView extends LoginPageState {
   ///Method that set _isTextFormCorrect based on valid logic
   void _isUserNameFormValidated() {
     setState(() {
-      if (_txtFieldCtrlr.text.length < 5 || !RegExp(r'^[a-zA-Z]+$').hasMatch(_txtFieldCtrlr.text))
+      if (_txtFieldCtrlr.text.length < 5 ||
+          !RegExp(r'^[a-zA-Z]+$').hasMatch(_txtFieldCtrlr.text))
         _isTextFormCorrect = false;
       else
         _isTextFormCorrect = true;
@@ -39,102 +40,106 @@ class LoginPageView extends LoginPageState {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("PapRocker"),
-      ),
-      body: Center(
-        //TODO: nice background with gradient
-        child: Form(
-          key: formNameKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Text(
-                  "Please enter your name",
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 40.0, right: 40.0),
-                child: TextFormField(
-                  autovalidate: true,
-                  autocorrect: false,
-                  maxLength: 30,
-                  keyboardType: TextInputType.text,
-                  validator: (nameValue) {
-                    if (nameValue.length < 5) {
-                      return "Name is too short!";
-                    }
-                    if (!RegExp(r'^[a-zA-Z]+$').hasMatch(nameValue)) {
-                      return "Name shouldn't contain any special characters!";
-                    }
-                  },
-                  controller: _txtFieldCtrlr,
-                  focusNode: _textFieldFcsNode,
-                  decoration: InputDecoration(
-                    hintText: "Player name",
-                    contentPadding: EdgeInsets.all(15),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                  ),
-                  onFieldSubmitted: (String nameValue) {
-                    changePlayerName(_txtFieldCtrlr.text);
-                  },
-                  onSaved: (String nameValue) {
-                    changePlayerName(_txtFieldCtrlr.text);
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Text(
-                  "Select your sex:",
-                  style: TextStyle(
-                    fontSize: 20,
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          const Color.fromRGBO(147, 165, 207, 1),
+          const Color.fromRGBO(228, 239, 233, 1),
+        ], begin: Alignment.topRight, end: Alignment.bottomLeft)),
+        child: Center(
+          child: Form(
+            key: formNameKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    "Please enter your name",
+                    style: TextStyle(fontSize: 20),
                   ),
                 ),
-              ),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Radio(
-                      value: PlayerSex.Male,
-                      onChanged: (sexValue) {
-                        changePlayerSex(sexValue);
-                      },
-                      groupValue: userSex,
+                Padding(
+                  padding: const EdgeInsets.only(left: 40.0, right: 40.0),
+                  child: TextFormField(
+                    autovalidate: true,
+                    autocorrect: false,
+                    maxLength: 20,
+                    textAlign: TextAlign.center,
+                    keyboardType: TextInputType.text,
+                    validator: (nameValue) {
+                      if (nameValue.length < 5) {
+                        return "Name is too short!";
+                      }
+                      if (!RegExp(r'^[a-zA-Z]+$').hasMatch(nameValue)) {
+                        return "Name shouldn't contain any special characters!";
+                      }
+                    },
+                    controller: _txtFieldCtrlr,
+                    focusNode: _textFieldFcsNode,
+                    decoration: InputDecoration(
+                      hintText: "Player name",
+                      contentPadding: EdgeInsets.all(15),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
                     ),
-                    Text("Male"),
-                    Radio(
-                      value: PlayerSex.Female,
-                      onChanged: (sexValue) {
-                        changePlayerSex(sexValue);
-                      },
-                      groupValue: userSex,
-                    ),
-                    Text("Female")
-                  ],
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  MaterialButton(
-                    child: Text("Select randomly"),
-                    onPressed: () {
-                      print("generate random hero");
+                    onFieldSubmitted: (String nameValue) {
+                      changePlayerName(_txtFieldCtrlr.text);
+                    },
+                    onSaved: (String nameValue) {
+                      changePlayerName(_txtFieldCtrlr.text);
                     },
                   ),
-                  MaterialButton(
-                    child: Text("Done"),
-                    onPressed: _isTextFormCorrect ? createNewPlayer : null,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    "Select your sex:",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
                   ),
-                ],
-              )
-            ],
+                ),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Radio(
+                        value: PlayerSex.Male,
+                        onChanged: (sexValue) {
+                          changePlayerSex(sexValue);
+                        },
+                        groupValue: userSex,
+                      ),
+                      Text("Male"),
+                      Radio(
+                        value: PlayerSex.Female,
+                        onChanged: (sexValue) {
+                          changePlayerSex(sexValue);
+                        },
+                        groupValue: userSex,
+                      ),
+                      Text("Female")
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    MaterialButton(
+                      child: Text("Select randomly"),
+                      onPressed: () {
+                        print("generate random hero");
+                      },
+                    ),
+                    MaterialButton(
+                      child: Text("Done"),
+                      onPressed: _isTextFormCorrect ? createNewPlayer : null,
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
