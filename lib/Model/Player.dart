e0d7908530c2600f5d98abedf84828d5fa745b30
paulkastel <pkastelik@poczta.fm>
2019-05-30 16:23:59 +0200

    True fantasy player name generator implemented

--- lib/Model/Player.dart
+++ lib/Model/Player.dart
@@ -10,6 +10,54 @@ class Player {
     playerSex = userSex;
   }
 
+  static final List<String> _partNameBegin = [
+    "Ar",
+    "Nei",
+    "Korn",
+    "Go",
+    "Zer",
+    "Fod",
+    "Kas",
+    "Pr",
+    "Wed",
+    "Mod",
+    "Hei",
+    "Bof",
+    "Cu",
+    "Ci",
+    "Zy"
+  ];
+  static final List<String> _partNameMiddle = [
+    "og",
+    "bra",
+    "n",
+    "o",
+    "roj",
+    "asa",
+    "zz",
+    "hon",
+    "u",
+    "ij",
+    "des",
+    "yh",
+    "du",
+    "kh"
+  ];
+  static final List<String> _partNameEnd = [
+    "nig",
+    "ing",
+    "x",
+    "a",
+    "r",
+    "op",
+    "s",
+    "c",
+    "ag",
+    "sin",
+    "me",
+    "us"
+  ];
+
   String _name;
 
   String get playerName => _name;
@@ -21,10 +69,9 @@ class Player {
 
   static String generateRandomPlayerName() {
     Random generator = new Random();
-    if (generator.nextBool())
-      return "NameE";
-    else
-      return "PlaerSex";
+    return _partNameBegin[generator.nextInt(_partNameBegin.length)] +
+        _partNameMiddle[generator.nextInt(_partNameMiddle.length)] +
+        _partNameEnd[generator.nextInt(_partNameEnd.length)];
   }
 
   static PlayerSex generateRandomPlayerSex() {
